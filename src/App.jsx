fd314705cb9bc203a5c4a51ddefbae43bbb3a491
Raji Olatunde Qamardeen <programmingwithOlatunderaji67@gmail.com>
2023-11-19 22:25:57 +0100

    API_Card

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Userlist from './Components/Userlist'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <Userlist/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/UserListCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const UserListCard = ({image, username}) => {
+  return (
+    <div className="card">
+        <div className="gitImage">
+            <img src={image} alt={username} />
+        </div>
+        <div>
+            <h2 className='gitName'>{username}</h2>
+        </div>
+    </div>
+  )
+}
+
+export default UserListCard
